1665404769f5bf71f6d425161b75ef5140bbd561
roborock <bbs61@hotmail.com.tr>
2022-10-29 14:49:02 +0300

    Glided Rose

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -2,11 +2,13 @@ export class Item {
   name: string;
   sellIn: number;
   quality: number;
+  category: string;
 
-  constructor(name, sellIn, quality) {
+  constructor(name, sellIn, quality, category) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
+    this.category = category;
   }
 }
 
@@ -22,7 +24,13 @@ export class GildedRose {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
+            if(this.items[i].name=="Conjured Mana Cake"){
+              this.items[i].quality = this.items[i].quality - 2
+            }
+            else
+            {
+              this.items[i].quality = this.items[i].quality - 1
+            }
           }
         }
       } else {
@@ -31,13 +39,16 @@ export class GildedRose {
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
+                this.items[i].quality = this.items[i].quality + 2
               }
             }
             if (this.items[i].sellIn < 6) {
               if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
+                this.items[i].quality = this.items[i].quality + 3
               }
+            }
+            if(this.items[i].sellIn == 0) {
+              this.items[i].quality = 0
             }
           }
         }
--- TypeScript/test/golden-master-text-test.ts
+++ TypeScript/test/golden-master-text-test.ts
@@ -1,30 +1,31 @@
 import { Item, GildedRose } from '../app/gilded-rose';
 
 const items = [
-  new Item("+5 Dexterity Vest", 10, 20), //
-  new Item("Aged Brie", 2, 0), //
-  new Item("Elixir of the Mongoose", 5, 7), //
-  new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-  new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+  new Item("+5 Dexterity Vest", 10, 20,"Vest"), //
+  new Item("Aged Brie", 2, 0,"Book"), //
+  new Item("Elixir of the Mongoose", 5, 7,"Elixir"), //
+  new Item("Sulfuras, Hand of Ragnaros", 0, 80,"Weapon"), //
+  new Item("Sulfuras, Hand of Ragnaros", -1, 80,"Weapon"),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20,"Other"),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49,"Other"),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49,"Other"),
   // this conjured item does not work properly yet
-  new Item("Conjured Mana Cake", 3, 6)];
+  new Item("Conjured Mana Cake", 3, 6,"Food")];
 
 
 const gildedRose = new GildedRose(items);
 
-let days: number = 2;
+let days: number = 3;
+
 if (process.argv.length > 2) {
-    days = +process.argv[2];
-  }
+  days = +process.argv[2];
+}
 
 for (let i = 0; i < days; i++) {
   console.log("-------- day " + i + " --------");
-  console.log("name, sellIn, quality");
+  console.log("name, sellIn, quality , category");
   items.forEach(element => {
-    console.log(element.name + ' ' + element.sellIn + ' ' + element.quality);
+    console.log(element.name + ' ' + element.sellIn + ' ' + element.quality + ' ' + element.category);
 
   });
   console.log();
